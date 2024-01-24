e9afa96b332331ccb8c17920e66de8337e375834
zabitaguliyeva <7644@holbertonstudents.com>
2024-01-24 06:19:19 -0800

    Alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ascii_code in range(ord('a'), ord('z')+1):
     alpha_letters = chr(ascii_code)
-    print("{}".format(alpha_letters), end = "")
+    print("{}".format(alpha_letters), end="")
