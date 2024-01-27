5b800a9682467452bdc9e69e60cb09b1028c3507
zabitaguliyeva <7644@holbertonstudents.com>
2024-01-27 04:26:32 -0800

    Added : end of the sentence

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif len(argv) == 2:
         print("{} argument:".format(len(argv) - 1))
     else:
-        print("{} arguments".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
