a0027a57a6edabf930fea416b0a02f250c3bc211
zabitaguliyeva <7644@holbertonstudents.com>
2024-01-30 06:23:20 -0800

    Number of keys

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    return (0 if not isinstance(a_dictionary, dict)
+    else len(a_dictionary) + sum(number_keys(val) for val in a_dictionary.values()))
+
