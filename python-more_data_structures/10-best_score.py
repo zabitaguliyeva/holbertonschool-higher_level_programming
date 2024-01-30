f088b243172ddc3848cdc251af5297089ec09df2
zabitaguliyeva <7644@holbertonstudents.com>
2024-01-30 08:49:26 -0800

    Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(zip(a_dictionary.keys(), a_dictionary.values()))[0]
+        return max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
     else:
         return None
