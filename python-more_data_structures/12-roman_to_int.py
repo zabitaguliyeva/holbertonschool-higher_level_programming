e4c5a15d3659777abee2fdf49d0e5c697a2f5586
zabitaguliyeva <7644@holbertonstudents.com>
2024-01-31 04:02:51 -0800

    Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -10,11 +10,13 @@ def roman_to_int(roman_string):
             "M": 1000
             }
     num = 0
+    if not isinstance(roman_string, str) and roman_string is not None:
+        return 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            num += roman_dict[roman_string[i]] - roman_dict[roman_string[i - 1]] * 2
+        if i > 0 and roman_dict[roman_string[i]] >\
+                roman_dict[roman_string[i - 1]]:
+            num += roman_dict[roman_string[i]]\
+                            - roman_dict[roman_string[i - 1]] * 2
         else:
             num += roman_dict[roman_string[i]]
     return num
-
-
