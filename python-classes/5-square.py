fed359b1a3747f42600ca77af4a09a4c8dc448fe
zabitaguliyeva <7644@holbertonstudents.com>
2024-02-04 06:20:07 -0800

    Printing a square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -33,5 +33,7 @@ class Square:
 
     def my_print(self):
         """that prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
         for i in range(self.__size):
             print("#" * self.__size)
