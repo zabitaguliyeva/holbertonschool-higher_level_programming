8092e34f939318a321ba08d47768fba0f4a94204
zabitaguliyeva <7644@holbertonstudents.com>
2024-02-04 05:58:05 -0800

    Access and update private attribute

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Access and update private attribute"""
+
+
+class Square:
+    """Why a getter and setter?"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
