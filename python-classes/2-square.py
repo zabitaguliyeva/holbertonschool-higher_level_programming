bffea9e1120c4e275c5af97a4a5c65a22539d94c
zabitaguliyeva <7644@holbertonstudents.com>
2024-02-04 01:33:53 -0800

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Private instance attribute: size
+   Instantiation with optional size: def __init__(self, size=0):
+   size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
+   if size is less than 0, raise a ValueError exception with the message size must be >= 0"""
+
+
 class Square:
+    """Size validation"""
     def __init__(self, size=0):
         self.__size = size
