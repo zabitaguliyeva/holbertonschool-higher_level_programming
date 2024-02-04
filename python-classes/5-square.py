aa76f97375e513155f9857848f3b86d8a7adf964
zabitaguliyeva <7644@holbertonstudents.com>
2024-02-04 06:14:52 -0800

    Printing a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Access and update private attribute"""
+
+
+class Square:
+    """Why a getter and setter?"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """that prints in stdout the square with the character #"""
+        for i in range(self.__size):
+            print("#" * self.__size)
