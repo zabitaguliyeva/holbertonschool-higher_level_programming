8d6bb299315c65c1ba53c8c3ee7c7bf89ab3d2b6
zabitaguliyeva <7644@holbertonstudents.com>
2024-02-06 05:26:20 -0800

    Eval is magic

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -49,4 +49,4 @@ class Rectangle:
         return "\n".join(mylist)
 
     def __repr__(self):
-        return(f"Rectangle({self.__width}, {self.__height})")
+        return(f"Rectangle({self.__width},{ self.__height})")
