39aa02191c0820b7c32cd0960e9750edb439a615
zabitaguliyeva <7644@holbertonstudents.com>
2024-02-08 06:09:04 -0800

    3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Same class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """isinstance returns true both inherit and subclas"""
+
+    return isinstance(obj, a_class)
