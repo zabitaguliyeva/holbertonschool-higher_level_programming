5ddc46ca054115982426711306d2e56439eb1cf2
zabitaguliyeva <7644@holbertonstudents.com>
2024-02-08 23:56:50 -0800

    Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 def inherits_from(obj, a_class):
     """Function"""
 
-    print(type(obj) is not a_class and isinstance(obj, a_class))
+    return(type(obj) is not a_class and isinstance(obj, a_class))
