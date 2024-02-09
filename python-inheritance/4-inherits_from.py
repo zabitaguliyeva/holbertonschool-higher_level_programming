96bcac878f429dfe0a2e403834e9d0eb57807cb5
zabitaguliyeva <7644@holbertonstudents.com>
2024-02-08 23:59:53 -0800

    4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Function"""
-    return(type(obj) is not a_class and isinstance(obj, a_class))
+    return (type(obj) is not a_class and isinstance(obj, a_class))
