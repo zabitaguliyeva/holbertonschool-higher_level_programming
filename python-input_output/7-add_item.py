c9cf59ba25aaeeb3f476e9028d99b9e29f9712c7
zabitaguliyeva <7644@holbertonstudents.com>
2024-02-11 01:18:49 -0800

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """Load, add, save"""
 import sys
-import json
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv
 filename = "add_item.json"
-
-with open(filename, "a+", encoding="utf-8") as f:
-    my_list = []
-    my_list.append(args[1:])
-    save_to_json_file(my_list, filename)
+try:
     load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
+
+save_to_json_file(args[1:], filename)
+
