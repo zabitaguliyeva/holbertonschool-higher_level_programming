e695fe7bda3af49e28072e987f70e5ab6d621e21
zabitaguliyeva <7644@holbertonstudents.com>
2024-02-12 04:44:36 -0800

    1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Base class"""
+
+
+class Base():
+    """class Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Function"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
