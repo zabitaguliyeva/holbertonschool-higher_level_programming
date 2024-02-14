47a1544ab7b9defa55683a194080cd54a366249e
zabitaguliyeva <7644@holbertonstudents.com>
2024-02-14 02:36:44 -0800

    12

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,9 +91,9 @@ class Rectangle(Base):
     def to_dictionary(self):
         """Dictionary"""
         return {
-                "id" : self.id,
-                "width" : self.width,
-                "height" : self.height,
-                "x" : self.x,
-                "y" : self.y
+                "id": self.id,
+                "width": self.width,
+                "height": self.height,
+                "x": self.x,
+                "y": self.y
                 }
