1ca7f3950e114fa62ef49dacf670740345ce9c20
zabitaguliyeva <7644@holbertonstudents.com>
2024-02-14 04:23:30 -0800

    14

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -15,6 +15,7 @@ class Base():
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod        
     def to_json_string(list_dictionaries):
         """Json to string"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
