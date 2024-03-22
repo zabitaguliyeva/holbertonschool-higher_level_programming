e033ea5d2f1e7be5d32e7539d0786a4eea4bcfc7
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-22 14:25:34 +0000

    16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- a script that lists all records of the table
-SELECT score, name FROM second_table WHERE name != 'NULL'
+SELECT score, name FROM second_table WHERE name != 'NULL' ORDER BY score DESC
