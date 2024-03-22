cac3d05cf0d8ce18050f6db24c48b7432b72a939
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-22 11:51:08 +0000

    5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,5 @@
+-- Select the hbtn_0c_0 database
+USE hbtn_0c_0;
+
 -- a script that prints the following description of the table first_table
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+SHOW CREATE TABLE first_table;
