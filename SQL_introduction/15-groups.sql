f732bffb44932944532aa18c75a885e355f5da46
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-22 14:16:16 +0000

    15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
- -- a script that lists the number of records with the same score
+-- a script that lists the number of records with the same score
 SELECT score, COUNT(score) as number
 FROM second_table
 GROUP BY score
