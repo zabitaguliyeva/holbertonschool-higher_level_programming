650473c0d3b4acd77dddc6db11c7d07eff7e1398
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-23 12:26:17 +0000

    7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- a script that creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	UNIQUE(ID),
+	PRIMARY KEY(ID),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+)
