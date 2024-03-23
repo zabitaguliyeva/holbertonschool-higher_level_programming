672835df1d3834108f8428563b7b07294ecae1fd
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-23 10:38:08 +0000

    0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- a script that lists all privileges of the MySQL users
-SELECT *
-FROM mysql.user
-WHERE user IN ('user_0d_1', 'user_0d_2');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
