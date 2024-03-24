ae40bf64195b7b8771f5c88d7407df2841a5f6da
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-24 21:22:09 +0400

    4

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,6 @@ if __name__ == "__main__0":
         user = sys.argv[1],
         password = sys.argv[2],
         db = sys.argv[3],
-        searched = sys.argv[4]
     )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
