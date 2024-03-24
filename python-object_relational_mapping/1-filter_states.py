6fcd3201756c6dd97c01be29495a3d2b80bee0a8
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-24 14:06:48 +0000

    1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Filter states
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "main":
+
+    db = MySQLdb.connect(
+            host="localhost",
+            username = sys.argv[1],
+            password = sys.argv[2], 
+            db = sys.argv[3],
+            port = 3306
+            )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
