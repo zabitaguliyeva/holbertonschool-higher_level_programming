a8ca257376a91eda2cf1a93df1929db89754a153
zabitaguliyeva <7644@holbertonstudents.com>
2024-03-24 14:27:26 +0000

    1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,11 +7,11 @@ import MySQLdb
 import sys
 
 
-if __name__ == "main":
+if __name__ == "__main__":
 
     db = MySQLdb.connect(
             host="localhost",
-            username = sys.argv[1],
+            user = sys.argv[1],
             password = sys.argv[2], 
             db = sys.argv[3],
             port = 3306
